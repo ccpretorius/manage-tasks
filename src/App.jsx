87ef5fa535d2460ab4ef124990ce6f0ef95795ff
ccpretorius <84196590+ccpretorius@users.noreply.github.com>
2024-02-10 19:39:49 +0100

    collecting user input with Refs and Forwarded Refs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
-    selectedProjectId: undefined, // undefined indicates that now project has been or are being selected
+    selectedProjectId: undefined, // undefined indicates that no project has been or are being selected
     projects: [],
   });
 
@@ -18,10 +18,26 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects],
+        newProject,
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }

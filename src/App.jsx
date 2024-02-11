a3a887d7f1af341b3bbd859f9c77cc2bc73953b1
ccpretorius <84196590+ccpretorius@users.noreply.github.com>
2024-02-11 17:37:35 +0100

    create a project and updating the UI with a list + a bug fix in projects array

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,16 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        //id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
-        projects: [...prevState.projects],
-        newProject,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
       };
     });
   }
@@ -44,7 +46,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
       {content}
     </main>
   );

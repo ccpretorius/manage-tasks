0b37bc8bc1cc0e5e816ea6efe0379b89562781b6
ccpretorius <84196590+ccpretorius@users.noreply.github.com>
2024-02-15 22:47:51 +0100

    activating the cancel button with props

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import Input from "./Input.jsx";
 import Modal from "./Modal.jsx";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
 
   const title = useRef();
@@ -38,7 +38,9 @@ export default function NewProject({ onAdd }) {
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
+              Cancel
+            </button>
           </li>
           <li>
             <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>
